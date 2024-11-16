078c488969b5bda1b1cec96c79d0f9503e5f0095
Lskrzypc <louis.skrzypczak@student.junia.com>
2024-11-16 18:35:48 +0100

    refactor(infrastructure): Update resource group module variables and remove unused virtual network module (yet)

--- infrastructure/modules/vnet/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "vnet_id" {
-  description = "ID du Virtual Network"
-  value       = azurerm_virtual_network.vnet.id
-}
-
-output "subnet_id" {
-  description = "ID de la Subnet"
-  value       = azurerm_subnet.subnet.id
-}
--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,33 +1,16 @@
-# Resource group related outputs
-
+# RESOURCE_GROUP OUTPUTS
 output "resource_group_name" {
-  description = "Nom du groupe de ressources"
-  value       = module.resource_group.rg_name
+  value = module.resource_group.resource_group_name
 }
 
 output "resource_group_id" {
-  description = "ID du groupe de ressources"
-  value       = module.resource_group.rg_id
+  value = module.resource_group.resource_group_id
 }
 
-output "resource_group_location" {
-  description = "Localisation du groupe de ressources"
-  value       = module.resource_group.location
+output "physical_location" {
+  value = module.resource_group.physical_location
 }
 
-output "resource_group_message" {
-  description = "Message de création du groupe de ressources"
-  value       = module.resource_group.message
-}
-
-# Virtual Network related outputs
-
-output "vnet_id" {
-  description = "ID du Virtual Network"
-  value       = module.vnet.vnet_id
-}
-
-output "subnet_id" {
-  description = "ID de la Subnet"
-  value       = module.vnet.subnet_id
+output "success_message" {
+  value = module.resource_group.success_message
 }
